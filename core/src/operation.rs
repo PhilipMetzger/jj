f1ede103fcd5928b01043cd8ca362fe3b00e1697
Philip Metzger <philipmetzger@bluewin.ch>
2026-05-04 21:34:22 +0200

    core: Lower `Backend`, `Index` and their dependencies from `jj-lib` into it
    They all need to be moved in one step since they depend on eachother. 
    It also moves the `jj-lib-proc-macro` crate since it also is a
    dependent. And since I'm moving these files around let's finally finish writing their 
    documentation.
    
    TODO: `Workspace`,`WorkingCopy` and `Index`
    also split it into individual commits

--- core/src/operation.rs
+++ core/src/operation.rs
@@ -1,4 +1,4 @@
-// Copyright 2026 The Jujutsu Authors
+// Copyright 2020 The Jujutsu Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 
 //! Contains [`Operation`] a thin view over the [`op_store::Operation`] containing some helpers to
 //! make processing easier.
